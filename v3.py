84e30fedb9e5b72fd21a1448f158c2a64f0f57b8
rohith359 <srohith2584@gmail.com>
2024-10-17 14:10:45 +0530

    Create v3.py

---
+++ v3.py
@@ -0,0 +1,16 @@
+...
+3) 
+The program takes a number from the user and generates a dictionary that contains numbers (between 1 and n) in the form (x,x*x). 
+Problem Solution:
+1. Take a number from the user and store it in a separate variable.
+2. Declare a dictionary and using dictionary comprehension initialize it to values keeping the number between 1 to n as the key and the square of the number as their values.
+3. Print the final dictionary.
+4. Exit.
+Sample I/O:
+5
+Sample O/P:
+{1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+...
+n = int(input())
+squared_dict = {x: x * x for x in range(1, n + 1)}
+print(squared_dict)
